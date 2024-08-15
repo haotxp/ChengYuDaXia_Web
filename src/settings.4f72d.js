440a502ca4cd1001f45e8d0ef4066969baca981f
haotxp <623769279@qq.com>
2024-08-15 14:31:17 +0800

    add

---
+++ src/settings.4f72d.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/FirstScene/FirstScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"d0143",main:"46d6c"}};
